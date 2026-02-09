46a2ec977ccc674eaaf2972c3b83a5bbf7cc5b79
a14157 <43444358+a14157@users.noreply.github.com>
2026-02-09 12:59:14 +0000

    Fix pipeline

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,11 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  // Build into docs/ so we can publish project pages from the main branch using the docs folder
+  build: {
+    outDir: "docs",
+    emptyOutDir: true,
+  },
   server: {
     host: "::",
     port: 8080,
